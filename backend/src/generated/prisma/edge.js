b5721f598f364da2c71b2335281e4295fac0adf5
Harshit-oli <oliharshit89@gmail.com>
2025-12-17 17:44:46 +0530

    add start script for production

--- backend/src/generated/prisma/edge.js
+++ backend/src/generated/prisma/edge.js
@@ -259,11 +259,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://myuser:harshit@999012@localhost:5432/postgres"
+        "value": null
       }
     }
   },
