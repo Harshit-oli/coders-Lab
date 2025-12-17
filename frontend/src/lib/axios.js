903a1bd221dadad6da27e6199a428b5fb4be7be6
Harshit-oli <oliharshit89@gmail.com>
2025-12-17 18:18:54 +0530

    fix axios baseURL using env

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
-import axios from "axios"
+import axios from "axios";
 
-export const exiosInstance=axios.create({  // create ka kaam yha ye hota hai hme baar baar axios.get,axios.post ke liye alag se ye sab nhi bnana padta hum seedhe jis file ke andar axios.create kiya hai uske baad .get,.post lga sakte hai jisse ki dry principle evaluate nhi hota hai
-    baseURL:import.meta.env.MODE === "development" ? "http://localhost:8000/api/v1" : "/api/v1",
+export const exiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL, // yaha development/production automatically pick hoga
     withCredentials: true,
-})
+});
+
+
+// create ka kaam yha ye hota hai hme baar baar axios.get,axios.post ke liye alag se ye sab nhi bnana padta hum seedhe jis file ke andar axios.create kiya hai uske baad .get,.post lga sakte hai jisse ki dry principle evaluate nhi hota hai
