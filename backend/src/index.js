6761c60201c69b94fbc5bcd24579b045b179bd28
Harshit-oli <oliharshit89@gmail.com>
2025-12-17 21:35:34 +0530

    change somthing

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const PORT=process.env.PORT;
 app.use(express.json());
 app.use(
     cors({
-      origin: "https://coders-lab.vercel.app",
+      origin: "http://localhost:5173",
       credentials: true,
     })
   );
